b20b3d820b213fc874d9ef0da07abbc41dfd7a6e
lvanswee23-sys <l.vanswee23@gmail.com>
2025-12-06 12:17:53 +0100

    Loads to dragon fight after replay castle scene (error because of arduino)

--- Assets/Scripts/SceneEnder.cs
+++ Assets/Scripts/SceneEnder.cs
@@ -7,15 +7,20 @@ public class SceneEnder : MonoBehaviour
     public CurtainsOpenSceneOne curtain1;
     public CurtainsOpenSceneOne curtain2;
     public CameraSwitcher cameraSwitcher;
+    private bool replay;
+    public string nextSceneName;    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        
+        replay = false;
     }
 
     public void EndScene()
     {
+        if(replay){
+            SceneManager.LoadScene(nextSceneName);
+        }
+        replay = true;
         curtain1.ResetForTimeline();
         curtain2.ResetForTimeline();
         cameraSwitcher.SwitchCameraDisplays();
